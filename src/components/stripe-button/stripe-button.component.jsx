4dd348773f49fc6c6a9dddf12bbea55b385796f6
sornasundaram <sorna.sundaram@risksense.com>
2021-07-10 16:43:28 +0530

    updating stripe integration and updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JBaXjSG1ScwfDESR0RZMLZ4u58r7O3uONZVvyBzaFWwBJjk98BPWlZiK5WbfJUGodWwi3Iq35bhSoULvTNtNQLC00sh0iSHGS';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
