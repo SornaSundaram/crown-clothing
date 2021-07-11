5bc5559cfd52b6abc458d24358b9036a08ffae7e
sornasundaram <sorna.sundaram@risksense.com>
2021-07-11 10:49:06 +0530

    removed shop data from local and added hoc pattern

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,6 +39,24 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollections.reduce((acc, collection) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
