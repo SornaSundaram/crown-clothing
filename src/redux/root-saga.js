ccbcb0bcd6c3679514f4ffeebbf7b14c75f1ed8d
sornasundaram <sorna.sundaram@risksense.com>
2021-07-13 22:04:34 +0530

    added root saga and replaced redux thunk for shop collection

---
+++ src/redux/root-saga.js
@@ -0,0 +1,7 @@
+import { all, call } from 'redux-saga/effects';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
+
+export default function* rootSage() {
+  yield all([call(fetchCollectionsStart)]);
+}
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from '@redux-saga/core/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
 import {
   firestore,
   convertCollectionsSnapshotToMap,
@@ -22,7 +22,7 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTION_START,
     fetchCollectionsAsync,
   );
