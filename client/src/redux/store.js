58decf399b44e2a24b585019d54252822dd6d55d
sornasundaram <sorna.sundaram@risksense.com>
2021-07-19 20:31:00 +0530

    added code splitting and wrapped routes with lazy loading

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -20,4 +20,6 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
-export default { store, persistStore };
+const finalExport = { store, persistStore };
+
+export default finalExport;
